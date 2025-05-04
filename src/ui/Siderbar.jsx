a714330c9c6b1a3d8d854afbac299345020cba33
Cleve-keem <belloharyourmidey@gmail.com>
2025-05-04 04:39:35 -0500

    built sidebar features and main nav

--- src/ui/Siderbar.jsx
+++ src/ui/Siderbar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "../ui/Logo";
+import MainNav from "../ui/MainNav";
+
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
 
   grid-row: 1/-1;
-  /* grid-column: 2; */
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 export default function Siderbar() {
-  return <StyledSidebar>SIDEBAR</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
