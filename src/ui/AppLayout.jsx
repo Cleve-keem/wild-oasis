aaaaaac43397c6eafa0ee3e3f4faf96702cdf1e6
Cleve-keem <belloharyourmidey@gmail.com>
2025-05-07 17:56:56 -0500

    added notifications

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,16 +5,24 @@ import Siderbar from "./Siderbar";
 import styled from "styled-components";
 
 const Main = styled.main`
-    background-color: var(--color-grey-50);
-    padding: 4rem 4.8rem 6.4rem;
-`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 const Grid = styled.div`
-    display: grid;
-    grid-template-columns: 26rem 1fr;
-    grid-template-rows: auto 1fr;
-    height: 100vh;
-`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
 
 export default function AppLayout() {
   return (
@@ -23,7 +31,9 @@ export default function AppLayout() {
       <Siderbar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </Grid>
   );
